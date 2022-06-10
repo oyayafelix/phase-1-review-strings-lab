84d236a2f37bbdddff4f723e33f1cf51cfce3c46
oyayafelix <oyayafelix@gmail.com>
2022-06-10 15:00:47 +0300

    commit tree 09c49c38b2050cc25612838f85815c0fb6fa1b07

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
